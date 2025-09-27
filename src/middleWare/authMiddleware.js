ce782b017e97562db6e380232f60700c517788ff
sayedmahmoud223 <sbendary977@gmail.com>
2025-09-27 21:08:10 +0300

    update auth_middleware

--- src/middleWare/authMiddleware.js
+++ src/middleWare/authMiddleware.js
@@ -10,13 +10,13 @@ export const roles = {
 
 export const authMiddleware = (roles = []) => {
     return (async (req, res, next) => {
-        const { token } = req.headers
-        if (!token) return next(new ResError("Authentication required. Please login.", 401));
-        const payload = methodsWillUsed.verifyToken({ token })
+        const { accessToken } = req.headers
+        if (!accessToken) return next(new ResError("Authentication required. Please login.", 401));
+        const payload = methodsWillUsed.verifyToken({ token: accessToken })
         const user = await userModel.findById({ _id: payload.id })
         if (!user) return next(new ResError("User not found", 400));
         if (!roles.includes(user.role)) return next(new ResError("Access denied. Unauthorized role.", 403));
         req.user = { id: user.id, role: user.role, email: user.email }
         return next()
     })
-}
+}   
